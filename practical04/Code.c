1f77728983a759887ea1e4f7e98a9e50b0f145aa
joelkariyalil <joelkariyalil@gmail.com>
2024-10-09 00:20:33 +0100

    ADD: Create a function/subroutine that calculates the area under the curve of tan(x)from 0→60degrees using the Trapezoidal Ru

---
+++ practical04/Code.c
@@ -0,0 +1,47 @@
+#include<stdio.h>
+#include<math.h>
+
+float tanCalc(int N){
+
+    float a = 0;
+    float b = 60;
+
+    float bRad = (M_PI * b) / 180;
+
+    float totalSum = tan(a) + tan(bRad);
+
+    printf("\n\nSum (Before Loop):\t\t%f\n", totalSum);
+
+    for(int i=5; i<60; i=i+5)
+        totalSum += 2*tan(i*M_PI/180);
+
+    printf("Sum (After Loop):\t\t%f\n", totalSum);
+
+    totalSum *= ((bRad - a)/(2 * N));
+    printf("Sum (Final Value):\t\t%f", totalSum);
+
+    float absDiff = fabs(totalSum - logf(2));
+    float relDiff = fabs(1 - (totalSum/logf(2)));
+
+    printf("\nAbsolute Difference:\t\t%f", absDiff);
+    printf("\nRelative Difference:\t\t%f\n\n", relDiff);
+
+    return totalSum;
+}
+
+int main() {
+
+    // How do I make this public?
+    float arr[12];
+
+    for(int i=0;i<12;i++){
+    arr[i] = 2*tan(((i+1)*5)*M_PI/180);
+    }
+
+    printf("%f", tanCalc(10));
+    printf("%f", tanCalc(20));
+    printf("%f", tanCalc(30));
+    printf("%f", tanCalc(40));
+    
+    return 0;
+}
